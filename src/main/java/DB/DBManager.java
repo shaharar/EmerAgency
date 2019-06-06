bdd441535817f34caff39af6dce95d2fa8c90a4c
shaharar <shaharar@post.bgu.ac.il>
2019-06-07 00:14:02 +0300

    maven2

---
+++ src/main/java/DB/DBManager.java
@@ -0,0 +1,50 @@
+package DB;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class DBManager {
+
+
+/*    public ArrayList<String> getCategories() {
+
+
+        return result;
+    }
+
+    public ArrayList<String> getPolicemen() {
+
+    }
+
+    public ArrayList<String> getFiremen() {
+
+    }
+
+    public ArrayList<String> getParamedics() {
+
+    }
+
+    public void createEvent(String username, String title, Date publishDate, int updateID) {
+
+    }
+
+    public String getFirstUpdate() {
+
+    }
+
+    public String[] getEventsByCategory (String categoryName) {
+
+    }
+
+    public String[] getEvent (String eventID) {
+
+    }
+
+    public String getPermissionOfEvent (String username, String eventID) {
+
+    }
+
+    public String createUpdate (String username, String eventInfo, Date updateDate) {
+
+    }*/
+}
---
+++ src/main/java/View/View.java
@@ -0,0 +1,16 @@
+package View;
+
+import Controller.Controller;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class View implements Observer {
+
+    private Controller controller;
+
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+}
